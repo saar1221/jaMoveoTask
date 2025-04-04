c973c7529b37b4cf3335c777b4e70b225794b2fd
saar <saar1231@gmail.com>
2025-04-04 23:35:38 +0300

    defaultCorsOptions

--- backend/src/server.js
+++ backend/src/server.js
@@ -9,31 +9,25 @@ import router from "./routes/index.js";
 import { SocketIoService } from "./services/index.js";
 
 dotenv.config();
+const defaultCorsOptions = {
+  origin: "*",
+  methods: ["GET", "POST"],
+  allowedHeaders: ["Content-Type"],
+};
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server, {
-  cors: {
-    origin: "*",
-    methods: ["GET", "POST"],
-    allowedHeaders: ["Content-Type"],
-  },
-});
+
+const io = new Server(server, defaultCorsOptions);
 
 connectDB();
 SocketIoService.init(io);
 
 app.use(helmet());
-app.use(
-  cors({
-    origin: "*",
-    methods: ["GET", "POST"],
-    allowedHeaders: ["Content-Type"],
-  })
-);
 
 app.use(express.json());
 app.use(router);
+app.use(cors(defaultCorsOptions));
 
 const PORT = process.env.SERVER_PORT || 4001;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
